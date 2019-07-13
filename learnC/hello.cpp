27390c773f23f96e8259426b0ae2bcf144fd33bc
尘土 <1669691816@qq.com>
2019-07-13 09:53:20 +0800

    函数调用

--- learnC/hello.cpp
+++ learnC/hello.cpp
@@ -1,6 +1,19 @@
 #include<stdio.h>
 
+int max(int x, int y) {
+	if (x > y) {
+		return x;
+	}
+	else {
+		return y;
+	}
+}
+
 int main() {
-	printf("Hello... C");
+	int a, b;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	int c = max(a, b);
+	printf("最大值为%d", c);
 	return 0;
 }
