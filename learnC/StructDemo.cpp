49514eb18cb4cd44fe92d7bf5a7b08aaecdf7935
尘土 <1669691816@qq.com>
2019-07-13 19:05:48 +0800

    typedef重命名

--- learnC/StructDemo.cpp
+++ learnC/StructDemo.cpp
@@ -3,13 +3,22 @@
 
 void structDemo() 
 {
+	typedef struct Book
+	{
+		char author[20];
+		double price;
+	}Book;
 	struct Stu
 	{
 		char name[20];
-		int age;
+		int age = 18;
 	}stu;	
 	stu.age = 21;
 	strcpy_s(stu.name, "zhangsan");
 	printf("%s的年龄为：%d", stu.name, stu.age);
+	Book book;
+	strcpy(book.author, "莫言");
+	book.price = 25.0;
+	printf("%s的书要%f元", book.author, book.price);
 	return;
 }
