ef818992835dd4bb8016bca77c1589d5777fc224
& <1669691816@qq.com>
2020-01-15 15:32:44 +0800

    新建leetcode头文件和对应cpp文件

--- learnC/main.cpp
+++ learnC/main.cpp
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include<assert.h>
+#include<ctype.h>
 #include "global.h"
 
 int main()
 {
-	printf("ÇëÊäÈëÒ»¸ö×Ö·û´®:");
-	FlipString();
+	int a = 'c';
+	int b = '3';
+	int c = '\t';
+	printf("%d\n", isalnum(a));
+	printf("%d\n", isalnum(b));
+	printf("%d", isalnum(c));
 	return 0;
 }
